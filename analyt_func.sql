d0d4b4aaf7db3eb9db345e7658da991a106b89fb
vandaGordovskaya <vgordovskaya@gmail.com>
2021-09-13 23:27:32 +0300

    Fixed incorrectness

--- analyt_func.sql
+++ analyt_func.sql
@@ -1,14 +1,15 @@
 USE AdventureWorks2019
 GO 
--- 1.1 Убираем выбросы. Найти продукты за январь-2013 по сумме продаж без первых и последних 10%, используя таблицы
+-- 1.1 Remove ejection. Find Products in JAN-2013 by sales amount without the first and last 10%.
 -- Sales.SalesOrderHeader
 -- Sales.SalesOrderDetail
 -- Production.Product
-SELECT *
+SELECT Product,
+	   Total
 FROM (
-    SELECT DISTINCT sod.ProductID,
-           SUM(LineTotal) OVER(ORDER BY sod.ProductID) Total,
-		   NTILE(10) OVER(ORDER BY sod.ProductID) AS Parts
+    SELECT pp.Name AS Product,
+           SUM(sod.LineTotal) AS Total,
+		   NTILE(10) OVER(ORDER BY SUM(sod.LineTotal)) AS Parts
     FROM Production.Product AS pp
 	JOIN Sales.SalesOrderDetail AS sod
     ON pp.ProductID = sod.ProductID
@@ -16,39 +17,30 @@ FROM (
     ON sod.SalesOrderID = soh.SalesOrderID
     WHERE OrderDate >= '2013-01-01'
        AND OrderDate < '2013-02-01'
-    GROUP BY LineTotal, sod.ProductID, OrderDate
+    GROUP BY pp.Name
 	) AS t
 WHERE Parts BETWEEN '2' AND '9'
 ORDER BY Parts
-
 GO
--- 1.2 Найти самые дешёвые (цена по прайсу) продукты в каждой из субкатегорий продуктов 
--- (для тех продуктов, у которых субкатегория задана), 
--- используя таблицу Production.Product
-SELECT ProductSubcategoryID,
-	   ProductID,
-       MIN(ListPrice) OVER(PARTITION BY ProductSubcategoryID ORDER BY ProductSubcategoryID) AS CheapestPrice
-FROM Production.Product
-WHERE ProductSubcategoryID IS NOT NULL
+-- 1.2 Find the cheapest Products in each Subcategory of Products where Subcategory exists using Production.Product
+SELECT Name,
+       ListPrice 
+FROM(SELECT Name,
+		   RANK() OVER(PARTITION BY ProductSubcategoryID ORDER BY ListPrice) AS Level,
+	       ListPrice
+	FROM Production.Product
+	WHERE ProductSubcategoryID IS NOT NULL) AS temp
+WHERE Level=1
 GO
--- 1.3 Найти вторую по величине цену для горных велосипедов, используя таблицу Production.Product
-SELECT ProductID,
-       ProductSubcategoryID,
-	   MAX(MaxPrice) AS SndMaxPrice
-FROM (SELECT ProductID,
-       ProductSubcategoryID,
-       MAX(ListPrice) OVER(ORDER BY ListPrice) AS MaxPrice,
-	   DENSE_RANK() OVER(ORDER BY ListPrice DESC) AS CountRank
+-- 1.3 Find the second max value price of Mountain Bicycles using Production.Product
+SELECT DISTINCT ListPrice AS SndMaxPrice
+FROM (SELECT ListPrice,
+	   DENSE_RANK() OVER(PARTITION BY ProductSubcategoryID ORDER BY ListPrice DESC) AS CountRank
 	   FROM Production.Product
-	   WHERE ProductSubcategoryID = 1
-	   GROUP BY ListPrice, ProductID, ProductSubcategoryID) AS MaxPrices
+	   WHERE ProductSubcategoryID = 1) AS RangePrices
 WHERE CountRank = 2
-GROUP BY ProductID, ProductSubcategoryID, MaxPrice
 GO
-IF OBJECT_ID (N'Production.ufnFindTotalSalesByCategoryInYear', N'FN') IS NOT NULL  
-    DROP FUNCTION Production.ufnFindTotalSalesByCategoryInYear; 
-GO
--- 1.4 Посчитать продажи за 2013 год в разрезе Категорий, посчитать YoY метрику ((продажи-продажи_за_прошлый_год)\продажи), используя таблицы
+-- 1.4 Count YoY (sales-previous_year_sales)/sales
 -- Sales.SalesOrderHeader
 -- Sales.SalesOrderDetail
 -- Production.Product
@@ -57,10 +49,10 @@ GO
 WITH CategoryTotalBeYears_CTE
 AS
 (
-    SELECT DISTINCT pc.ProductCategoryID,
-           pc.Name as Category,
-		   YEAR(OrderDate) AS Years,
-	       SUM(LineTotal) OVER(Partition BY pc.ProductCategoryID ORDER BY pc.ProductCategoryID, YEAR(OrderDate)) AS TotalByCatInYear
+    SELECT pc.Name as Category,
+           YEAR(OrderDate) AS Years,
+	   SUM(LineTotal) AS CurrentTotal,
+           LAG(sum(sod.LineTotal)) OVER (ORDER BY pc.Name ,YEAR(soh.OrderDate)) PrevSales
 	FROM Production.ProductSubcategory as ps
 	JOIN Production.ProductCategory as pc 
 	ON ps.ProductCategoryID = pc.ProductCategoryID
@@ -71,33 +63,33 @@ AS
 	JOIN Sales.SalesOrderHeader AS soh
 	ON sod.SalesOrderID = soh.SalesOrderID
 	WHERE YEAR(OrderDate) IN ('2012', '2013')
-	GROUP BY pc.ProductCategoryID, pc.Name, YEAR(OrderDate), LineTotal
+	GROUP BY YEAR(OrderDate), pc.Name
 )
 SELECT *,
-       ((TotalByCatInYear - (LAG(TotalByCatInYear) OVER(PARTITION BY ProductCategoryID ORDER BY ProductCategoryID)))/TotalByCatInYear) AS YoY
+       (CurrentTotal - PrevSales)/CurrentTotal AS YoY
 FROM CategoryTotalBeYears_CTE
-ORDER BY ProductCategoryID
+WHERE Years='2013'
 GO
--- 1.5 Найти сумму максимальную заказа за каждый день января 2013, используя таблицы
+-- 1.5 Select the max sum of order for each day of JAN-2013
 -- Sales.SalesOrderHeader
 -- Sales.SalesOrderDetail
 SELECT DISTINCT OrderDate,
-       MAX(LineTotal) OVER(PARTITION BY DAY(OrderDate) ORDER BY OrderDate
-	   ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS MaxTotalForDay
+       MAX(LineTotal) OVER(PARTITION BY DAY(OrderDate)) AS MaxTotalForDay
 FROM Sales.SalesOrderHeader AS soh
 JOIN Sales.SalesOrderDetail AS sod
 ON soh.SalesOrderID = sod.SalesOrderID
-WHERE YEAR(OrderDate) = '2013' AND MONTH(OrderDate) = '01'
+WHERE YEAR(OrderDate) = '2013' 
+    AND MONTH(OrderDate) = '01'
 GROUP BY OrderDate, LineTotal
 ORDER BY OrderDate
 GO
--- 1.6 Найти товар, который чаще всего продавался в каждой из субкатегорий в январе 2013, используя таблицы
+-- 1.6 Find the most sold good in JAN-2013
 -- Sales.SalesOrderHeader
 -- Sales.SalesOrderDetail
 -- Production.Product
 -- Production.ProductSubcategory
-SELECT DISTINCT MAX(COUNT(pp.ProductID)) OVER(PARTITION BY pp.ProductSubcategoryID ORDER BY pp.ProductSubcategoryID) AS MaxTotalCount, 
-       pp.ProductSubcategoryID
+SELECT DISTINCT pps.Name, 
+       FIRST_VALUE(pp.Name) OVER(PARTITION BY pp.ProductSubcategoryID ORDER BY Count(*) DESC) PopularGood     
 FROM Sales.SalesOrderHeader AS soh
 JOIN Sales.SalesOrderDetail AS sod
 ON soh.SalesOrderID = sod.SalesOrderID
@@ -106,4 +98,4 @@ ON sod.ProductID = pp.ProductID
 JOIN Production.ProductSubcategory AS pps
 ON pp.ProductSubcategoryID = pps.ProductSubcategoryID
 WHERE YEAR(OrderDate) = '2013' AND MONTH(OrderDate) = '01'
-GROUP BY pp.ProductSubcategoryID, pp.ProductID, pp.Name
+GROUP BY pp.ProductSubcategoryID, pp.ProductID, pp.Name, pps.Name
